df573c3702c1b78d8ebaf13b8d72fb5b22242a2e
fcoterroba <fcoterroba@gmail.com>
2024-08-05 09:55:08 +0300

    Create hello world kata in cpp

---
+++ CPP/HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include <string>
+
+std::string greet() {
+  return "hello world!";
+}
+
+// Original kata: https://www.codewars.com/kata/523b4ff7adca849afe000035
+// My solution: https://www.codewars.com/kata/reviews/5e64ab1aba41250001567d47/groups/5e64ab1bba41250001567d4b
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CodeWars
- Repo to save all my solutions to Codewars' katas
+Repo to save all my solutions to Codewars' katas
+
+## Index
+- Monday -> [C++](https://github.com/fcoterroba/CodeWars/tree/main/CPP)
