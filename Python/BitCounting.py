79cd2bbec13378a0ce4ab5c304790821e5632994
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2024-10-15 09:50:29 +0200

    :tada: Start Python folder

---
+++ Python/BitCounting.py
@@ -0,0 +1,5 @@
+def count_bits(n):
+    return format(n, 'b').count('1')
+
+# Original kata: https://www.codewars.com/kata/526571aae218b8ee490006f4
+# My solution: https://www.codewars.com/kata/reviews/54909b9defb59728f600016f/groups/5f3bddbd9ece1900015529ae
