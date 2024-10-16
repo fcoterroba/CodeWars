7867b8cec299dc7d4961bf5b19c5b51243976cb5
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2024-10-16 10:01:02 +0200

    :tada: First kata on C#

---
+++ CS/MinNumber.cs
@@ -0,0 +1,12 @@
+using System.Linq;
+
+public class Kata 
+{
+    public static int FindSmallestInt(int[] args) 
+    {
+      return args.Min();
+    }
+}
+
+// Original kata: https://www.codewars.com/kata/55a2d7ebe362935a210000b2
+// My solution: https://www.codewars.com/kata/reviews/5614f9b317500e7ca3000029/groups/5615141a7060b9db5e0000d8
