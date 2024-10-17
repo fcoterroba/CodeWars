cb7a04400556de6786b5c848465d3495c48197e6
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2024-10-17 10:30:21 +0200

    :tada: Create Go folder

---
+++ Go/DoubleInteger.go
@@ -0,0 +1,8 @@
+package kata
+
+func DoubleInteger(i int) int {
+  return i*2
+}
+
+// Original kata: https://www.codewars.com/kata/53ee5429ba190077850011d4
+// My solution: https://www.codewars.com/kata/reviews/60432cc6042a6f0001fc7c6d/groups/60432cc6042a6f0001fc7c71
