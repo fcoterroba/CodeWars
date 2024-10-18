5fa8a1832286f37b4f3386d285d96355f1476b9b
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2024-10-18 15:30:29 +0200

    :tada: Create Rust folder

---
+++ Rust/OppositesAttract.rs
@@ -0,0 +1,6 @@
+fn lovefunc(flower1: u16, flower2: u16) -> bool {
+    return (flower1 + flower2) % 2 == 1
+}
+
+// Original kata: https://www.codewars.com/kata/555086d53eac039a2a000083
+// My solution: https://www.codewars.com/kata/reviews/62a945b27b85ea00014f2b97/groups/63041b2a43fc1900018d567b
