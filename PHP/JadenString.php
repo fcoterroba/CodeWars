d8749ca815d2e479ff14b63031e9dbb460f5c7ea
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2024-10-21 11:42:37 +0200

    Create JadenString.php

---
+++ PHP/JadenString.php
@@ -0,0 +1,8 @@
+<?php
+function toJadenCase(string $string): string
+{
+  return ucwords($string);
+}
+
+// Original kata: https://www.codewars.com/kata/5390bac347d09b7da40006f6
+// My solution: https://www.codewars.com/kata/reviews/578d41a0c6f53716a900009c/groups/64dcda28b329a60001fa3f3c
