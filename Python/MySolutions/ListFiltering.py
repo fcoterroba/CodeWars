7f463fadaf6132698e0a5f8657b4237905adaedf
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2024-12-04 10:16:13 +0100

    :tada: Create list filtering kata in Python

---
+++ Python/MySolutions/ListFiltering.py
@@ -0,0 +1,5 @@
+def filter_list(l):
+    return [item for item in l if isinstance(item, int)]
+
+# Original kata: https://www.codewars.com/kata/53dbd5315a3c69eed20002dd
+# My solution: https://www.codewars.com/kata/reviews/53dbd54c3721866025000604/groups/61d9b615b7cf9b00016d62ba
