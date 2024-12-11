4fd54e7cd2f8606e058da8a916a74997abb8f9c1
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2024-12-11 10:43:53 +0100

    :tada: Starting C# kata

---
+++ CPP/NumberToString/MySolution.cpp
@@ -0,0 +1,8 @@
+#include <string>
+
+std::string number_to_string(int num) {
+  return std::to_string(num);
+}
+
+// Original kata: https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+// My solution: https://www.codewars.com/kata/reviews/5d6ad6f3476ab3000132d23a/groups/5d6ad6f5476ab3000132d23e
