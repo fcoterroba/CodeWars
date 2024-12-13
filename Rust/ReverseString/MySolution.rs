6c9b58d36f37a2fd3044a3bae2eb5a0178ff9bcf
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2024-12-13 11:11:54 +0100

    :tada: Starting Rust kata

---
+++ Rust/ReverseString/MySolution.rs
@@ -0,0 +1,6 @@
+fn solution(phrase: &str) -> String {
+    return phrase.chars().rev().collect::<String>();
+}
+
+// Original kata: https://www.codewars.com/kata/5168bb5dfe9a00b126000018
+// My solution: https://www.codewars.com/kata/reviews/59e4fe2b6782e9a576001655/groups/5a0e0baf94c21bc7700005e2
