2eb32b37312ac71d6522b6a4c9e400c2de7ebc87
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-01-10 09:06:22 +0100

    :tada: Init C# katas

---
+++ CS/BooleanToString/MySolution.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class kata
+{
+  public static string BooleanToString(bool b)
+  {
+    return b.ToString();
+  }
+}
+
+// Solution's link: https://www.codewars.com/kata/reviews/5984142bf79c740ef6000136/groups/63d25cb92cb9c7000101d421
