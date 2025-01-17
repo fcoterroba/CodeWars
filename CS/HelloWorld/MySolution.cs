0eac0233e6e8b4b316949bbca610afb1e7a5069f
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-01-17 12:53:23 +0100

    :sparkles: Hello world in C#

---
+++ CS/HelloWorld/MySolution.cs
@@ -0,0 +1,9 @@
+public static class Kata
+{
+  public static string greet(){
+    return "hello world!";
+  }
+}
+
+// Original kata: https://www.codewars.com/kata/523b4ff7adca849afe000035
+// My solution: https://www.codewars.com/kata/reviews/5518bd8c4854f4c77b000019/groups/5c9110e97201680001f09e6e
