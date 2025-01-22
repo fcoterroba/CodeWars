7a73d5b76b3fb08eb711a75bda003e7d72e09db7
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-01-22 09:41:33 +0100

    :sparkles: Code under pressure #1 in Rust

---
+++ Rust/CodePressure1/MySolution.rs
@@ -0,0 +1,6 @@
+fn double_integer(n: i32) -> i32 {
+    return n*2;
+}
+
+// Original kata: https://www.codewars.com/kata/53ee5429ba190077850011d4
+// My solution: https://www.codewars.com/kata/reviews/5f849074e74ad0000151cb92/groups/5f84b085e74ad0000151cf74
