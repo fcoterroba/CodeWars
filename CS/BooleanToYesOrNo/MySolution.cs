2fffb4b75c9cc3e39765ee8653418230dc9ba5ef
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-01-24 08:53:37 +0100

    :sparkles: Boolean to yes or no in C#

---
+++ CS/BooleanToYesOrNo/MySolution.cs
@@ -0,0 +1,12 @@
+using System;
+
+public static class Kata
+{
+  public static string boolToWord(bool word)
+  {
+    return word ? "Yes" : "No";
+  }
+}
+
+// Original kata: https://www.codewars.com/kata/53369039d7ab3ac506000467
+// My solution: https://www.codewars.com/kata/reviews/5516e34e236c88afb300002c/groups/627f0f2131a327000125aa60
