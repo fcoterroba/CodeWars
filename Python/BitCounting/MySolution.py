0732cd7fd556124b6dbdb668eccd8ee1fabc3c06
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-01-27 10:54:31 +0100

    :sparkles: Bit counting kata in Python

---
+++ Python/BitCounting/MySolution.py
@@ -0,0 +1,5 @@
+def count_bits(n):
+    return bin(n).count('1')
+
+# Original kata: https://www.codewars.com/kata/526571aae218b8ee490006f4
+# My solution: https://www.codewars.com/kata/reviews/54909b9defb59728f600016f/groups/54909b9defb59728f6000171 
