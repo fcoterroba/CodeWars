201a9e2bed434ff49d5574d54697b8f53d9131ab
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-01-30 10:34:39 +0100

    :sparkles: Create reverse string kata in C++

---
+++ CPP/ReverseString/MySolution.cpp
@@ -0,0 +1,10 @@
+using namespace std ; 
+
+string reverseString (string str )
+{
+  reverse(str.begin(), str.end()); 
+  return str;
+}
+
+// Original kata: https://www.codewars.com/kata/5168bb5dfe9a00b126000018
+// My solution: https://www.codewars.com/kata/reviews/5ab3d480dc7a3210e5001f92/groups/631c94e8e73c050001276dab
