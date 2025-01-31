33d7afcd87b66410e7ca7a3291cc1d86508859b5
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-01-31 12:01:59 +0100

    :sparkles: Create DNA to RNA kata in C#

---
+++ CS/DNAtoRNA/MySolution.cs
@@ -0,0 +1,12 @@
+namespace Converter {
+  public class Converter
+  {
+    public string dnaToRna(string dna)
+    {
+      return dna.Replace('T', 'U');
+    }
+  }
+}
+
+// Original kata: https://www.codewars.com/kata/5556282156230d0e5e000089
+// My solution: https://www.codewars.com/kata/reviews/576090f9c41463b57800000a/groups/576090fac41463b57800000e
