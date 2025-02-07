0e3f7d75a81bfa362303222d16e452eda30795e5
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-02-07 13:11:19 +0100

    :sparkles: String to number kata in C#

---
+++ CS/StringToNumber/MySolution.cs
@@ -0,0 +1,10 @@
+using System;
+  public class Kata
+  {
+    public static int StringToNumber(String str) {
+      return Int32.Parse(str);
+  }
+}
+
+// Original kata: https://www.codewars.com/kata/544675c6f971f7399a000e79
+// My solution: https://www.codewars.com/kata/reviews/5607eeffddc52e59dd0000ca/groups/61d5cf6e8450cc0001075650
