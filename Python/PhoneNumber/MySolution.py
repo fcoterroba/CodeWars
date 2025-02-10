32d351fb1613a87d49bc39018c414acd96860ad7
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-02-10 10:18:29 +0100

    :sparkles: Create phone number kata in Python

---
+++ Python/PhoneNumber/MySolution.py
@@ -0,0 +1,5 @@
+def create_phone_number(n):
+    return f"({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}"
+
+# Original kata: https://www.codewars.com/kata/525f50e3b73515a6db000b83
+# My solution: https://www.codewars.com/kata/reviews/59b1a938182024506b00081d/groups/5b2978a7be728dfb89000683
