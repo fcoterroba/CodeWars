fbd10fcc091c256e5b66f9c32fcac30fad65ef24
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-02-12 09:18:45 +0100

    :sparkles: Create number to string kata in Rust

---
+++ Rust/NumberToString/MySolution.rs
@@ -0,0 +1,7 @@
+fn number_to_string(i: i32) -> String {
+    let result = i.to_string();
+    return result;
+}
+
+// Original kata: https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+// My solution: https://www.codewars.com/kata/reviews/5824a9f0847707bbff000005/groups/607e3e6055db560001269135
