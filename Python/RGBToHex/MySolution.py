8933258e66046ab97b5f1378a378c5c963390f4f
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-02-17 16:27:48 +0100

    :sparkles: RGB to Hex kata in Python

---
+++ Python/RGBToHex/MySolution.py
@@ -0,0 +1,5 @@
+def rgb(r, g, b):
+    return f'{max(0, min(255, r)):02X}{max(0, min(255, g)):02X}{max(0, min(255, b)):02X}'
+
+# Original kata: https://www.codewars.com/kata/513e08acc600c94f01000001
+# My solution: https://www.codewars.com/kata/reviews/54bd821b518b8d73630000a0/groups/5ef0a5a747358c0001712ed2
