a12a3c2b9b668a0d0718c053fbaa2f23b658a4f7
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-02-26 09:10:51 +0100

    :sparkles: Terminal game move in Rust

---
+++ Rust/GrasshoperTerminalGameMove/MySolution.rs
@@ -0,0 +1,6 @@
+fn move_hero(position: u32, roll: u32) -> u32 {
+    position + roll*2
+}
+
+// Original kata: https://www.codewars.com/kata/563a631f7cbbc236cf0000c2
+// My solution: https://www.codewars.com/kata/reviews/59e5ef95810fc72dc9000723/groups/59e5ef95810fc72dc9000727
