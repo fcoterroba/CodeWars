d71aeef0fd2f83a38f86ec3b1227e1720730706c
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-03-05 19:03:36 +0100

    :sparkles: Returning strings kata on Rust

---
+++ Rust/ReturningString/MySolution.rs
@@ -0,0 +1,6 @@
+fn greet(name: &str) -> String {
+     format!("Hello, {} how are you doing today?", name)
+}
+
+// Original kata: https://www.codewars.com/kata/55a70521798b14d4750000a4
+// My solution: https://www.codewars.com/kata/reviews/5f7854a67d74a9000169ac7b/groups/5f78552e8ef12000018aac2f
