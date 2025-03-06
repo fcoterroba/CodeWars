1011df8c79854f632982d8dfb7d72b0cbe18c0d2
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-03-06 09:24:57 +0100

    :sparkles: Create grasshoper messi on C++

---
+++ CPP/GrasshoperMessi/MySolution.cpp
@@ -0,0 +1,6 @@
+int goals (int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+// Original kata: https://www.codewars.com/kata/55f73be6e12baaa5900000d4
+// My solution: https://www.codewars.com/kata/reviews/5d2a1fcdd6e947000141ffc2/groups/5d2a1fced6e947000141ffc8
