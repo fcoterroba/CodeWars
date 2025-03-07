c8dacd23e07655404511028f382b587c40a1fb6b
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-03-07 08:56:15 +0100

    :sparkles: Get character ascii in C#

---
+++ CS/GetCharacterAscii/MySolution.cs
@@ -0,0 +1,10 @@
+public class Kata
+{
+    public static char GetChar(int charcode)
+    {
+      return (char)charcode;
+    }
+}
+
+// Original kata: https://www.codewars.com/kata/55ad04714f0b468e8200001c
+// My solution: https://www.codewars.com/kata/reviews/59a3e79e79216dfa0b002dd0/groups/59a3e7b779216dfa0b002dd4
