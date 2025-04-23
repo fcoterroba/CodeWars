0c43c4e72b5ff689cf7d21de5895a77f652cfb4b
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-04-23 09:12:35 +0200

    :sparkles: Create messi goals kata in Rust

---
+++ Rust/MessiGoals/MySolution.rs
@@ -0,0 +1,8 @@
+static la_liga_goals: u32 = 43;
+static champions_league_goals: u32 = 10;
+static copa_del_rey_goals: u32 = 5;
+
+static total_goals: u32 = la_liga_goals + champions_league_goals + copa_del_rey_goals;
+
+// Original kata: https://www.codewars.com/kata/55ca77fa094a2af31f00002a
+// My solution: https://www.codewars.com/kata/reviews/5f277e48d07dc30001f72bde/groups/5f277e48d07dc30001f72be2
