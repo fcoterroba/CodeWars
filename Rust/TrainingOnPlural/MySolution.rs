cf5263678d4f5a7938a6e45ab1b2f05d97b33c79
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-05-07 09:35:57 +0200

    :sparkles: Create plural kata in Rust

---
+++ Rust/TrainingOnPlural/MySolution.rs
@@ -0,0 +1,6 @@
+fn plural (n: f64) -> bool {
+    n != 1.0
+}
+
+// Original kata: https://www.codewars.com/kata/52ceafd1f235ce81aa00073a
+// My solution: https://www.codewars.com/kata/reviews/626ac20f902ebf0001f3d0d1/groups/626ac20f902ebf0001f3d0d5
