967952fab5006c444feb7158728d75c3b2ee8d9b
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-05-15 06:58:39 +0200

    :sparkles: Create square function kata in C++

---
+++ CPP/FunctionSquare/MySolution.cpp
@@ -0,0 +1,6 @@
+int square(int x) {
+    return x * x;
+}
+
+// Original kata: https://www.codewars.com/kata/523b623152af8a30c6000027
+// My solution: https://www.codewars.com/kata/reviews/5fb8385dfc3d1200018c29cc/groups/5fb8d4a78b5cad0001301944
