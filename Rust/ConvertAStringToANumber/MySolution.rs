56d0f818453b906c47e1c66d9db4bfc3850c94c6
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-05-28 09:28:23 +0200

    feat(rust kata): convert a string to a number

---
+++ Rust/ConvertAStringToANumber/MySolution.rs
@@ -0,0 +1,7 @@
+fn string_to_number(s: &str) -> i32 {
+  let my_int: i32 = s.parse().unwrap();
+  my_int
+}
+
+// Original kata: https://www.codewars.com/kata/544675c6f971f7399a000e79
+// My solution: https://www.codewars.com/kata/reviews/5824a81d3043839d8b00004c/groups/5faef8fbecf7210001f9005f
