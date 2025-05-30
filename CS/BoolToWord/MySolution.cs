601a861c8dc32dd1dc0005430443399eea21b548
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-05-30 09:35:15 +0200

    feat(cs kata): bool to string

---
+++ CS/BoolToWord/MySolution.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static string boolToWord(bool word)
+  {
+    return word ? "Yes" : "No";
+  }
+}
+
+// Original kata: https://www.codewars.com/kata/53369039d7ab3ac506000467/train/csharp
+// My solution: https://www.codewars.com/kata/reviews/5516e34e236c88afb300002c/groups/55172c95236c8807e0000504
