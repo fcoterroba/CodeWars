fc0ed5f8168d3d3fc7abf60f7fe6a54507ead705
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-06-02 09:12:20 +0200

    feat(cpp kata): bool to word

---
+++ CPP/BoolToWord/MySolution.cpp
@@ -0,0 +1,7 @@
+std::string bool_to_word(bool value)
+{
+  return value ? "Yes" : "No";
+}
+
+// Original kata: https://www.codewars.com/kata/53369039d7ab3ac506000467
+// My solution: https://www.codewars.com/kata/reviews/575fb0db77f0495393000053/groups/575fdbf1b9e171420a001393
