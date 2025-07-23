7aef467fc82f4b4175b0cee29736a8ef2444a4a1
fcoterroba <fcoterroba@gmail.com>
2025-07-23 08:20:49 +0200

    feat(rs kata): hello world

---
+++ Rust/HelloWorld/MySolution.rs
@@ -0,0 +1,6 @@
+fn greet() -> &'static str {
+    "hello world!"
+}
+
+// Original kata: https://www.codewars.com/kata/523b4ff7adca849afe000035
+// My solution: https://www.codewars.com/kata/reviews/5d7ab8e114c1b9000111dadb/groups/5d7ab8e614c1b9000111dadf
