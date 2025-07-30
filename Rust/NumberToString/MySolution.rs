c5b824c994bbb5ff761fca265717cec64fd706b7
fcoterroba <fcoterroba@gmail.com>
2025-07-30 09:19:06 +0200

    feat(rust kata): number to string

---
+++ Rust/NumberToString/MySolution.rs
@@ -0,0 +1,6 @@
+fn number_to_string(i: i32) -> String {
+    i.to_string()
+}
+
+// original kata: https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+// my solution: https://www.codewars.com/kata/reviews/5824a9f0847707bbff000005/groups/5824a9f3847707bbff000009
